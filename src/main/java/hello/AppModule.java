277f7bed41a822759f388258189459fe0e262d2e
Xavier Hanin <xavier.hanin@gmail.com>
2013-05-26 10:24:26 +0200

    hello sample generated by restx shell 'app new'

---
+++ src/main/java/hello/AppModule.java
@@ -0,0 +1,14 @@
+package hello;
+
+import com.google.common.base.Charsets;
+import restx.SignatureKey;
+import restx.factory.Module;
+import restx.factory.Provides;
+
+@Module
+public class AppModule {
+    @Provides
+    public SignatureKey signatureKey() {
+         return new SignatureKey("5341679063568601943 restx-samples-hello Hello e372a614-d7a5-477b-9e3f-40c182b9f34d".getBytes(Charsets.UTF_8));
+    }
+}
