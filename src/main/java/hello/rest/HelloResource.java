65271ac5690eace42bd9045bbe5865e004ecf4d5
Xavier Hanin <xavier.hanin@gmail.com>
2013-07-21 14:36:25 +0200

    upgrade to 0.2.8

--- src/main/java/hello/rest/HelloResource.java
+++ src/main/java/hello/rest/HelloResource.java
@@ -5,9 +5,11 @@ import org.joda.time.DateTime;
 import restx.annotations.GET;
 import restx.annotations.RestxResource;
 import restx.factory.Component;
+import restx.security.PermitAll;
 
 @Component @RestxResource
 public class HelloResource {
+    @PermitAll
     @GET("/message")
     public Message sayHello(String who) {
         return new Message().setMessage(String.format(
