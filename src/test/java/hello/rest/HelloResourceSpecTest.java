1e315ae66fc2f7c65446e01206dbda37b60cc61f
Xavier Hanin <xavier.hanin@gmail.com>
2013-09-16 18:38:24 +0200

    upgrade to restx 0.2.9

--- src/test/java/hello/rest/HelloResourceSpecTest.java
+++ src/test/java/hello/rest/HelloResourceSpecTest.java
@@ -1,15 +1,12 @@
 package hello.rest;
 
-import hello.AppServer;
 import org.junit.ClassRule;
 import org.junit.Test;
 import restx.tests.RestxSpecRule;
 
 public class HelloResourceSpecTest {
     @ClassRule
-    public static RestxSpecRule rule = new RestxSpecRule(
-            AppServer.WEB_INF_LOCATION,
-            AppServer.WEB_APP_LOCATION);
+    public static RestxSpecRule rule = new RestxSpecRule();
 
     @Test
     public void should_say_hello() throws Exception {
