2840fd29881c648735fc9d0bfb3d404471318cd2
Thomas Recloux <thomas.recloux@gmail.com>
2014-04-07 22:47:12 +0200

    Use JSR310 instead of JodaTime

--- src/main/java/hello/AppModule.java
+++ src/main/java/hello/AppModule.java
@@ -5,10 +5,17 @@ import restx.security.SignatureKey;
 import restx.factory.Module;
 import restx.factory.Provides;
 
+import java.time.Clock;
+
 @Module
 public class AppModule {
     @Provides
     public SignatureKey signatureKey() {
          return new SignatureKey("5341679063568601943 restx-samples-hello Hello e372a614-d7a5-477b-9e3f-40c182b9f34d".getBytes(Charsets.UTF_8));
+    }
+
+    @Provides
+    public Clock clock() {
+        return Clock.systemDefaultZone();
     }
 }
--- src/main/java/hello/rest/HelloResource.java
+++ src/main/java/hello/rest/HelloResource.java
@@ -1,19 +1,30 @@
 package hello.rest;
 
 import hello.domain.Message;
-import org.joda.time.DateTime;
 import restx.annotations.GET;
 import restx.annotations.RestxResource;
 import restx.factory.Component;
 import restx.security.PermitAll;
 
+import java.time.Clock;
+import java.time.LocalDateTime;
+
+import static java.time.format.DateTimeFormatter.ofPattern;
+
 @Component @RestxResource
 public class HelloResource {
+
+    private Clock clock;
+
+    public HelloResource(Clock clock) {
+        this.clock = clock;
+    }
+
     @PermitAll
     @GET("/message")
     public Message sayHello(String who) {
         return new Message().setMessage(String.format(
                 "hello %s, it's %s",
-                who, DateTime.now().toString("HH:mm:ss")));
+                who, LocalDateTime.now(clock).format(ofPattern(("HH:mm:ss")))));
     }
 }
