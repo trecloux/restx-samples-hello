13ab3b01863dd5006ba05d7df9abff82574b8979
Thomas Recloux <thomas.recloux@gmail.com>
2014-04-08 18:57:35 +0200

    Test Jsr310 types mapping

--- src/main/java/hello/domain/Message.java
+++ src/main/java/hello/domain/Message.java
@@ -1,7 +1,22 @@
 package hello.domain;
 
+import java.time.Duration;
+import java.time.LocalDate;
+
+import static java.time.temporal.ChronoUnit.SECONDS;
+
 public class Message {
     private String message;
+    private LocalDate localDate = LocalDate.now();
+    private Duration duration = Duration.of(3600, SECONDS);
+
+    public LocalDate getLocalDate() {
+        return localDate;
+    }
+
+    public Duration getDuration() {
+        return duration;
+    }
 
     public String getMessage() {
         return message;
